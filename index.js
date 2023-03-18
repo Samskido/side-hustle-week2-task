b12bcde3af08b105449611bb3d0497038aff8b14
Samskido <onyekweresamskido@gmail.com>
2023-03-18 19:43:58 +0100

    side-hustle-week2-task

--- index.js
+++
@@ -1,8 +0,0 @@
-<!-- Script for range input label -->
-    <script>
-      var rangeLabel = document.getElementById("range-label");
-      var experience = document.getElementById("experience");
-      function change() {
-      rangeLabel.innerText = experience.value + "K";
-      }
-    </script>
